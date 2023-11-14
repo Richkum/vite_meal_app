9a63211dab481d85fd31cca9c616050e9726d3d1
Richkum <kumatum2@gmail.com>
2023-11-14 16:33:44 +0100

    building

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,6 +4,46 @@ import "./Footer.css";
 const FooterSise = () => {
   return (
     <>
+      <div class="mob-footer">
+        <div class="mob-foot">
+          <div class="mob-col1">
+            <img class="mob-img" src="./public/images/Logo 2.png"></img>
+            <p class="mob-lastp">
+              "On the other hand, we denounce with righteous indignation and
+              dislike men who are so beguiled and demoralized by the charm of
+              pleasure of the moment
+            </p>
+          </div>
+          <select name="" id="select">
+            <option class="dropdown">Tastebite</option>
+            <option>About Us</option>
+            <option>Careers</option>
+            <option>Contact Us</option>
+            <option>Feedback</option>
+          </select>
+          <select name="" id="select">
+            <option class="dropdown">Legal</option>
+            <option>Terms</option>
+            <option>Conditions</option>
+            <option>Cookies</option>
+            <option>Copyright</option>
+          </select>
+          <select name="" id="select">
+            <option class="dropdown">Follow</option>
+            <option>Facebook</option>
+            <option>Twitter</option>
+            <option>Instagram</option>
+            <option>Youtube</option>
+          </select>
+          <div class="mob-icons">
+            <i class="fa-brands fa-facebook-f"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-youtube"></i>
+          </div>
+          <p class="lastp">2020 Tatebite-All rights reserved</p>
+        </div>
+      </div>
       <footer className="footer">
         <div className="foot">
           <div className="col1">
